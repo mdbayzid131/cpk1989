9de64f484852614ce57406903202bf2ed3651342
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-08-22 14:26:34 +0600

    refactor: reorganize project structure by moving documentation files into Note directory and adding new notification screen and assets

--- lib/module/notification/view/notification_screen.dart
+++ lib/module/notification/view/notification_screen.dart
@@ -101,12 +101,7 @@ class NotificationScreen extends GetView<NotificationController> {
                       // Date Group Header (e.g. "TODAY", "AUG 19")
                       SliverToBoxAdapter(
                         child: Padding(
-                          padding: EdgeInsets.fromLTRB(
-                            16.w,
-                            18.h,
-                            16.w,
-                            10.h,
-                          ),
+                          padding: EdgeInsets.fromLTRB(16.w, 18.h, 16.w, 10.h),
                           child: Text(
                             dateGroup.toUpperCase(),
                             style: GoogleFonts.dmSans(
@@ -123,25 +118,23 @@ class NotificationScreen extends GetView<NotificationController> {
                       SliverPadding(
                         padding: EdgeInsets.symmetric(horizontal: 16.w),
                         sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              final item = items[index];
-                              return Padding(
-                                padding: EdgeInsets.only(bottom: 10.h),
-                                child: _buildNotificationCard(item),
-                              );
-                            },
-                            childCount: items.length,
-                          ),
+                          delegate: SliverChildBuilderDelegate((
+                            context,
+                            index,
+                          ) {
+                            final item = items[index];
+                            return Padding(
+                              padding: EdgeInsets.only(bottom: 10.h),
+                              child: _buildNotificationCard(item),
+                            );
+                          }, childCount: items.length),
                         ),
                       ),
                     ];
                   }),
 
                 // Bottom Padding to ensure cards aren't obscured by bottom nav bar
-                SliverToBoxAdapter(
-                  child: SizedBox(height: 110.h),
-                ),
+                SliverToBoxAdapter(child: SizedBox(height: 110.h)),
               ],
             ),
           );
@@ -156,10 +149,7 @@ class NotificationScreen extends GetView<NotificationController> {
         gradient: const LinearGradient(
           begin: Alignment.centerRight,
           end: Alignment.centerLeft,
-          colors: [
-            Color(0xFF292A2D),
-            Color(0xFF1C1D21),
-          ],
+          colors: [Color(0xFF292A2D), Color(0xFF1C1D21)],
           stops: [0.2161, 0.5276],
         ),
         borderRadius: BorderRadius.circular(16.r),
@@ -180,9 +170,7 @@ class NotificationScreen extends GetView<NotificationController> {
               color: const Color(0xFF323236),
               borderRadius: BorderRadius.circular(12.r),
             ),
-            child: Center(
-              child: _buildNotificationSvgIcon(item.type),
-            ),
+            child: Center(child: _buildNotificationSvgIcon(item.type)),
           ),
 
           SizedBox(width: 12.w),
@@ -269,10 +257,7 @@ class NotificationScreen extends GetView<NotificationController> {
   Widget _buildSubtitleText(String subtitle) {
     // Parse words like "AED 3,200" or "AED 3,200." to bold them matching mockup
     final List<TextSpan> spans = [];
-    final RegExp regExp = RegExp(
-      r'(AED\s?[\d,]+(\.)?)',
-      caseSensitive: false,
-    );
+    final RegExp regExp = RegExp(r'(AED\s?[\d,]+(\.)?)', caseSensitive: false);
 
     int start = 0;
     for (final Match match in regExp.allMatches(subtitle)) {
@@ -369,109 +354,113 @@ class NotificationScreen extends GetView<NotificationController> {
                     borderRadius: BorderRadius.circular(16.r),
                     child: BackdropFilter(
                       filter: ImageFilter.blur(sigmaX: 18.0, sigmaY: 18.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: const Color(0x14FFFFFF), // #FFFFFF14 fill
-                          borderRadius: BorderRadius.circular(16.r),
-                          border: Border.all(
-                            color: Colors.white.withValues(alpha: 0.18),
-                            width: 1.0,
-                          ),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withValues(alpha: 0.35),
-                              blurRadius: 24,
-                              offset: const Offset(0, 10),
-                            ),
-                          ],
+                      child: CustomPaint(
+                        painter: RRectGlassBorderPainter(
+                          borderRadius: 16.r,
+                          strokeWidth: 1.0,
                         ),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              // 1. Mark as all read
-                              InkWell(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(16.r),
-                                  topRight: Radius.circular(16.r),
-                                ),
-                                onTap: () {
-                                  Navigator.of(dialogContext).pop();
-                                  controller.markAllAsRead();
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 16.w,
-                                    vertical: 14.h,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.done_all_rounded,
-                                        color: Colors.white,
-                                        size: 20.r,
-                                      ),
-                                      SizedBox(width: 12.w),
-                                      Expanded(
-                                        child: Text(
-                                          "Mark as all read",
-                                          style: GoogleFonts.dmSans(
-                                            fontSize: 14.sp,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-
-                              // Divider line
-                              Container(
-                                height: 1.0,
-                                color: Colors.white.withValues(alpha: 0.1),
-                              ),
-
-                              // 2. Delete all
-                              InkWell(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(16.r),
-                                  bottomRight: Radius.circular(16.r),
-                                ),
-                                onTap: () {
-                                  Navigator.of(dialogContext).pop();
-                                  controller.deleteAllNotifications();
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 16.w,
-                                    vertical: 14.h,
-                                  ),
-                                  child: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.delete_outline_rounded,
-                                        color: Colors.white,
-                                        size: 20.r,
-                                      ),
-                                      SizedBox(width: 12.w),
-                                      Expanded(
-                                        child: Text(
-                                          "Delete all",
-                                          style: GoogleFonts.dmSans(
-                                            fontSize: 14.sp,
-                                            fontWeight: FontWeight.w400,
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color(0x14FFFFFF), // #FFFFFF14 fill
+                            borderRadius: BorderRadius.circular(16.r),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withValues(alpha: 0.35),
+                                blurRadius: 24,
+                                offset: const Offset(0, 10),
                               ),
                             ],
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                // 1. Mark as all read
+                                InkWell(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(16.r),
+                                    topRight: Radius.circular(16.r),
+                                  ),
+                                  onTap: () {
+                                    Navigator.of(dialogContext).pop();
+                                    controller.markAllAsRead();
+                                  },
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 16.w,
+                                      vertical: 14.h,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/notifecation/mark.svg',
+                                          width: 18.r,
+                                          height: 18.r,
+                                          fit: BoxFit.contain,
+                                        ),
+                                        SizedBox(width: 12.w),
+                                        Expanded(
+                                          child: Text(
+                                            "Mark as all read",
+                                            style: GoogleFonts.dmSans(
+                                              fontSize: 14.sp,
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+
+                                // Divider line
+                                Container(
+                                  height: 1.0,
+                                  color: Colors.white.withValues(alpha: 0.1),
+                                ),
+
+                                // 2. Delete all
+                                InkWell(
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(16.r),
+                                    bottomRight: Radius.circular(16.r),
+                                  ),
+                                  onTap: () {
+                                    Navigator.of(dialogContext).pop();
+                                    controller.deleteAllNotifications();
+                                  },
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 16.w,
+                                      vertical: 14.h,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        SvgPicture.asset(
+                                          'assets/notifecation/delete.svg',
+                                          width: 18.r,
+                                          height: 18.r,
+                                          fit: BoxFit.contain,
+                                        ),
+                                        SizedBox(width: 12.w),
+                                        Expanded(
+                                          child: Text(
+                                            "Delete all",
+                                            style: GoogleFonts.dmSans(
+                                              fontSize: 14.sp,
+                                              fontWeight: FontWeight.w400,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -485,4 +474,42 @@ class NotificationScreen extends GetView<NotificationController> {
       },
     );
   }
+}
+
+/// Painter that draws a mathematically precise, crisp glass refraction border on rounded rectangles
+class RRectGlassBorderPainter extends CustomPainter {
+  final double borderRadius;
+  final double strokeWidth;
+
+  RRectGlassBorderPainter({required this.borderRadius, this.strokeWidth = 1.0});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final rect = Offset.zero & size;
+    final rrect = RRect.fromRectAndRadius(rect, Radius.circular(borderRadius));
+    final borderGradient = LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      colors: [
+        Colors.white.withValues(
+          alpha: 0.65,
+        ), // Top-Left glass highlight refraction
+        Colors.white.withValues(alpha: 0.08), // Subtle dim middle
+        Colors.white.withValues(
+          alpha: 0.55,
+        ), // Bottom-Right glass highlight refraction
+      ],
+      stops: const [0.0, 0.5, 1.0],
+    );
+
+    final paint = Paint()
+      ..strokeWidth = strokeWidth
+      ..style = PaintingStyle.stroke
+      ..shader = borderGradient.createShader(rect);
+
+    canvas.drawRRect(rrect.deflate(strokeWidth / 2), paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
